68062c455910d75fbbb0bd019b5e7463cda40536
mohammadkiaei <s.kiaei.s.m.m.k1380@gmail.com>
2022-10-30 15:33:31 +0330

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".btn");
+btn.addEventListener("mousemove", (e) => {
+    const xpos = e.pageX - e.target.offsetLeft;
+    const ypos = e.pageY - e.target.offsetTop;
+
+    e.target.style.setProperty("--x", `${xpos}px`);
+    e.target.style.setProperty("--y", `${ypos}px`);
+});
+
+// Developed By SeyedMohammadMahdi Kiaei
